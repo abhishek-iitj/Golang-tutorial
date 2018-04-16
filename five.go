63e869d2f4d03bd91bdbcfae1d85e8de0af18cb8
Abhishek Sah <abhi.sah.97@gmail.com>
2018-04-17 02:24:54 +0530

    Panic and recover basics

--- five.go
+++ five.go
@@ -8,12 +8,21 @@ import (
 
 var wg sync.WaitGroup
 
+func cleanup() {
+	defer wg.Done() //we really wanna make sure we defer to done
+	if r := recover(); r != nil {
+		fmt.Println("Recovered in cleanup", r)
+	}
+	//wg.Done()
+}
 func say(s string) {
-	defer wg.Done() //what if we dont actually get to get here so we need Defer.
-
+	defer cleanup()
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond * 100)
+		if i == 2 {
+			panic("Oh dear , it's a panic " + s)
+		}
 	}
 	// wg.Done() //what if we dont actually get to get here so we need Defer.
 
